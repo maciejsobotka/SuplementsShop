e99f17898db22ffb1bdf613c5bb3d2cdafaa214e
maciejsobotka <sobot92@tlen.pl>
2016-04-13 12:56:39 +0200

    Changed script execution order in SQLServerMessageCapture.

--- SQLServerMessageCapture/Program.cs
+++ SQLServerMessageCapture/Program.cs
@@ -19,12 +19,13 @@ namespace SQLServerMessageCapture
             try
             {
                 string workloadA = File.ReadAllText(@"D:\WorkLoadA.sql");
-                //string workloadB = File.ReadAllText(@"D:\WorkLoadB.sql");
+                string workloadB = File.ReadAllText(@"D:\WorkLoadB.sql");
+                string[] workloads = new string[2] { workloadA, workloadB };
                 string rsW1 = File.ReadAllText(@"D:\RestoreScriptW1.sql");
                 string rsW2 = File.ReadAllText(@"D:\RestoreScriptW2.sql");
                 string rsW3 = File.ReadAllText(@"D:\RestoreScriptW3.sql");
                 string[] rScripts = new string[3] { rsW1, rsW2, rsW3 };
-                string[] fileNames = new string[6] { @"D:\AW1.txt", @"D:\AW2.txt", @"D:\AW3.txt", @"D:\BW1.txt", @"D:\BW2.txt", @"D:\BW3.txt" };
+                string[] fileNames = new string[6] { @"D:\AW1.txt", @"D:\BW1.txt", @"D:\AW2.txt", @"D:\BW2.txt", @"D:\AW3.txt", @"D:\BW3.txt" };
 
                 string sqlConnectionString = @"Data Source=DESKTOP-4J8NAAL;Initial Catalog=SuplShop;Integrated Security=True;Pooling=False";
                 Stopwatch sw = new Stopwatch();
@@ -32,15 +33,15 @@ namespace SQLServerMessageCapture
                 conn.InfoMessage += new SqlInfoMessageEventHandler(Message);
                 Server server = new Server(new ServerConnection(conn));
                 sw.Start();
-                for(int i = 0; i < 2; ++i)
+                for(int i = 0; i < 3; ++i)
                 {
-                    for (int j = 0; j < 3; ++j) 
+                    for (int j = 0; j < 2; ++j) 
                     {
-                        file = new System.IO.StreamWriter(fileNames[i*3+j], true);
+                        file = new System.IO.StreamWriter(fileNames[i*2+j], true);
                         for (int k = 0; k < 10; ++k)
                         {
-                            server.ConnectionContext.ExecuteNonQuery(rScripts[j]);
-                            server.ConnectionContext.ExecuteNonQuery(workloadA);
+                            server.ConnectionContext.ExecuteNonQuery(rScripts[i]);
+                            server.ConnectionContext.ExecuteNonQuery(workloads[j]);
                         }
                         file.Close();
                     }
